48986dade3f6fb4e7e8bc0404809ce13ec80fefb
zkkl02840-byte <zkkl02840@gmail.com>
2025-09-04 13:04:27 +0530

    Add files via upload
    just click on the html file, Squareroot isnt working

---
+++ X4java.js
@@ -0,0 +1,16 @@
+function press(value) {
+  document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+
+function calculate() {
+  try {
+    let result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+  } catch {
+    document.getElementById('display').value = 'Error';
+  }
+}
